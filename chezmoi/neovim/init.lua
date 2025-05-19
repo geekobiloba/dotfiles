7ca749ed20fe6c5ef0999d8d0a4037f0700668ed
Ellam ByDefault <ellam.bydefault@gmail.com>
2025-05-19 21:18:14 +0700

    fix: tresitter for crystal

--- chezmoi/neovim/init.lua
+++ chezmoi/neovim/init.lua
@@ -72,7 +72,7 @@ end
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 parser_config.crystal = {
   install_info = {
-    url = "~/.tree-sitter-crystal",
+    url = "$HOME/.tree-sitter-crystal",
     files = {"src/parser.c", "src/scanner.c"},
     branch = "main",
   },
